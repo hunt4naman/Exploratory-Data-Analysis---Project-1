fd69bbce423fd6003cf7d9dfdd1161119bc0712b
hunt4naman <hunt4naman@yahoo.co.in>
2014-12-06 19:19:02 -0500

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,14 @@
+data<-read.table("household_power_consumption.txt",sep=";",header=T,na="?",stringsAsFactors=F)
+head(data)
+str(data)         
+attach(data)
+
+subset_Date<-subset_Date<-data[Date %in% c("1/2/2007","2/2/2007"),]
+str(subset_Date)
+
+# Converting the "subset_Date" variable in the numeric format to plot the histogram
+globalActivePower<-subset_Date$Global_active_power
+
+# Converting dates
+datetime <- strptime(paste(subset_Date$Date,subset_Date$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+head(datetime)
