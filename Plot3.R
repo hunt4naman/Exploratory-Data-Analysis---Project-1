fc20a98ebc3f20513f20e27c508e4f8029be3947
hunt4naman <hunt4naman@yahoo.co.in>
2014-12-06 19:19:25 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,4 @@
+
 data<-read.table("household_power_consumption.txt",sep=";",header=T,na="?",stringsAsFactors=F)
 head(data)
 str(data)         
@@ -12,3 +13,19 @@ globalActivePower<-subset_Date$Global_active_power
 # Converting dates
 datetime <- strptime(paste(subset_Date$Date,subset_Date$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 head(datetime)
+
+# Plotting the graph
+
+with(subset_Date, {
+  plot(datetime, Sub_metering_1, type='l', xlab="", ylab="Energy sub metering")
+  lines(datetime, Sub_metering_2, type='l', col='red')
+ lines(datetime, Sub_metering_3, type='l', col='blue')
+})
+legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd='1', col=c('black', 'red', 'blue'))
+
+# Copying the graph to png file with the given width and height
+
+dev.copy(png,file="plot3.png",width=480,height=480)
+
+#Closing the PNG device
+dev.off()
